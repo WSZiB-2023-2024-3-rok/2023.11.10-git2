c0ca1a2fd5b38af2db48de6f2cdbda25299d4cf7
Mateusz <bmatteo@interia.pl>
2023-11-10 13:10:40 +0100

    Update2

--- src/main/java/pl/edu/wszib/git2/App.java
+++ src/main/java/pl/edu/wszib/git2/App.java
@@ -3,5 +3,7 @@ package pl.edu.wszib.git2;
 public class App {
     public static void main(String[] args) {
         System.out.println("zmiana");
+
+        System.out.println("kolejna zmaina");
     }
 }
